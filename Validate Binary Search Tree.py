9d8debcba86ce0040bf927f8d04dba551dd60206
Sowndhariya Nandarajkumar <41890348+SoundNandu@users.noreply.github.com>
2020-02-27 19:21:09 -0800

    Update Validate Binary Search Tree.py

--- Validate Binary Search Tree.py
+++ Validate Binary Search Tree.py
@@ -57,5 +57,29 @@ class Solution:
         return self.__helper(root.left,root.val,minimum) and   self.__helper(root.right,maximum,root.val)
         
    space complexity O(1)
-   time complexity O(1)
-        
+   time complexity O(n)
+
+ Solution 2:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+        stack = []
+        prev = float('-inf')
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if prev >= root.val:
+                return False
+            prev = root.val
+            root = root.right
+        return True
